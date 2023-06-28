20dda4b689911f2d76f7bac480b5a61f796ec1d6
Bekzod <bek2k00@mail.ru>
2023-06-28 13:25:30 +0500

    complete hm

---
+++ hm1.py
@@ -0,0 +1,7 @@
+# Выведите в консоль таблицу умножения от 2х2 до 9х10 как на школьной тетрадке.
+
+for i in range(2, 10):
+    for j in range(2, 11):
+        result = i * j
+        print(f"{i} x {j} = {result}")
+    print()
